6d64d86dbc7ab33630d00293bff608d3ae262266
BTCPRL <33190493+BTCPRL@users.noreply.github.com>
2018-04-05 00:46:44 -0700

    Added try and except for import file

--- scripts/maya_core/maya_files.py
+++ scripts/maya_core/maya_files.py
@@ -2,6 +2,7 @@
 Library of functions to handle maya files. Keeps other modules 'free' from maya
 calls.
 """
+import os
 import pymel.core as pm
 import maya.cmds as cmds
 
@@ -9,7 +10,10 @@ def new_file():
     return pm.newFile(force = True)
 
 def import_file(file_path):
-    return pm.importFile(file_path, returnNewNodes = True)
+    try:
+        return pm.importFile(file_path, returnNewNodes = True)
+    except RuntimeError:
+        raise RuntimeError('File {} does not exist'.format(file_path))
 
 def save_file(file_path = None):
     return pm.saveFile()
