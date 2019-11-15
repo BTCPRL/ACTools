3d1696a8cf0e2b986f13aff7cd844337ee7cbc60
BTCPRL <33190493+BTCPRL@users.noreply.github.com>
2019-11-14 23:01:58 -0800

    Reorganized files

---
+++ builder/builder.py
@@ -0,0 +1,259 @@
+#Python imports
+import os
+import sys
+import imp
+import json
+
+#Maya imports
+import maya.cmds as cmds
+
+#CARF imports
+from CARF.data import global_data as g_data
+
+# from CARF.scripts import session
+from CARF.scripts.maya_core import maya_files
+from CARF.scripts.maya_core import controls
+
+class Builder(object):
+	""" 
+	Handles the build process of a given asset, requires a session object with
+	paths information. It will create a Rig object based on each asset's build
+	code.
+	"""
+	def __init__(self, session_obj):
+		
+		self.session_obj = session_obj
+		self.build_file = '%s_build.py' % session_obj.asset_name
+		self.stage = 'init'
+		self.rig_base = False
+		self.template_data = None
+		#Dynamically imports and reload [asset_name]_build.py
+		self.asset_module = imp.load_source(
+			'imported_asset_module',
+			os.path.join(session_obj.paths['asset'],self.build_file)
+		)
+
+	def set_stage(self, stage):
+		"""Changes current stage of the builder.
+		Needed for preventing undesired exports/imports
+		"""
+		if stage not in g_data.builder_stages:
+			raise Exception('%s is not a valid stage for the builder')
+		else:
+			self.stage = stage
+
+	def initialize_rig(self):
+		""" Creates an instance of the rig object
+		"""
+		#Creates a Rig object from the imported module
+		self.rig = self.asset_module.Asset(
+			asset_name = self.session_obj.asset_name
+		)
+
+	def configure_rig(self):
+		"""TODO: Still not too sure what's gonna happen here
+		"""
+		self.rig.configure_rig()
+
+	def setup_rig_base(self):
+		""" Builds base groups and root controller. 
+		Sets rig_base attribute
+		"""
+		self.rig.build_base_grps()
+		self.rig.build_root()
+		self.rig_base = True
+
+	def import_geo(self):
+		"""Imports the asset geo to the scene. 
+		If there's a rig base, it parents the imported geo under GEO_GRP
+		"""
+		new_nodes = maya_files.import_file(self.session_obj.paths['geo'])
+
+		if self.rig_base:
+			cmds.parent(self.session_obj.asset_name, self.rig.geo_grp)
+
+		return new_nodes
+
+	def build_template(self):
+		""" Call to rig's template build.
+		Will build each component's tempalte
+		"""
+		path = self.session_obj.paths['template']
+		template_file = os.path.join(path, 'template.json')
+		if os.path.isfile(template_file):
+			self.import_template_data()
+		self.rig.build_template()
+	
+	def assemble_rig(self):
+		""" Wrapper for the rig build method.
+		Builds all components and then executes the asset's build code
+		"""
+		self.rig.assemble_components()
+		self.rig.build()
+	
+	def finalize_rig(self):
+		""" Wrapper for the rig build method.
+		Builds all components and then executes the asset's build code
+		"""
+		self.set_stage('finalize')
+		self.rig.final_touches()
+	
+	def build_rig_template(self):
+		"""Builds the rig template
+		Sets the current stage to template and forces a new maya scene
+		TODO: some of these are just direct calls to rig.method(). If so, why
+		not just use the rig calls? at the end of the day, the user will be
+		editing a class that inherits from rig, no need to add extra steps to
+		builder since the user will not have direct access to them...right?
+		"""
+		self.set_stage('template')
+
+		maya_files.new_file() #New scene
+		
+		self.initialize_rig() #Instanciates rig object
+
+		self.configure_rig() #Stuff happens here TODO: define said stuff
+
+		self.setup_rig_base() #Creates base groups and root
+
+		self.import_geo() #Imports the geo and parents it under the rig
+
+		self.build_template() #Creates components templates
+
+	def get_template_data(self):
+		""" Creates a dictionary with each component's template data
+		"""
+		template_data = {}
+		for comp_name in self.rig.components.keys():
+			template_data[comp_name] = \
+							self.rig.components[comp_name].get_template_data()
+		return template_data
+
+	def export_template_data(self):
+		""" Gets current template data, saves it in a json file
+		"""
+		if self.stage is 'template':
+			data = self.get_template_data()
+			path = os.path.join(self.session_obj.paths['template'], 
+																'template.json')
+			with open(path, 'w') as file:
+				json.dump(data, file, indent = 4, sort_keys = True)
+				print '### Template data succesfully extracted! ###'
+		else:
+			print "###Warning: Not in the template stage,"\
+												"can't export template data"
+
+	def import_template_data(self):
+		""" Loads existing template data from template.json file
+		"""
+		path = os.path.join(self.session_obj.paths['template'], 'template.json')
+		if os.path.isfile(path):
+			#Loads json into variable
+			try:
+				with open(path, 'r') as file:
+					template_data = json.load(file)
+			except ValueError:
+				raise Exception('There was a problem reading tempalte.json'\
+										'check that it jas a valid json format')
+			#Stores variable into rig object
+			self.rig.set_template_data(template_data)
+		else:
+			raise Exception('No template data found')
+
+	def build_rig(self):
+		"""Builds the final rig.
+		This will create a new scene and a new instance of the rig object. Don't
+		use this if building step by step.
+		Executed steps:
+			Makes a new scene
+			Creates base groups
+			Imports geos
+			Assembles all the rig components
+			Binds geometry
+			Finalizes the rig
+		"""
+		self.set_stage('build')
+
+		maya_files.new_file() #New scene
+		
+		self.initialize_rig() #Instanciates rig object
+
+		self.configure_rig() #Stuff happens here TODO: define said stuff
+		
+		self.import_template_data() #Load saved tempalte data
+		
+		self.setup_rig_base() #Creates base groups and root
+
+		self.import_geo() #Imports the geo and parents it under the rig
+
+		self.assemble_rig() #Builds all the components for the rig
+
+		self.import_rig_ctrls() #Import saved ctrls shapes
+
+		self.finalize_rig() #Lock attributes, hide grps
+
+		print '###  YUP  ###'
+	
+	def pre_publish(self):
+		""" Preps the rig for publishing
+		Sets most nodes to not historically interesting.
+		Runs QC check (TODO)
+		"""
+		#TODO: implement
+		pass
+		
+	def export_rig_ctrls_shapes(self):
+		""" Creates a json file containing the shape info for the rig ctrls
+		"""
+		#Get all ctrls under CONTROLS_GRP
+		all_ls = cmds.listRelatives(self.rig.ctrls_grp, 
+													ad = 1, type = 'transform')
+		ctrls_ls = [x for x in all_ls if x.split('_')[-1] == 'CTR']
+		
+		#Store their shape information 
+		rig_ctrls = {}
+		for ctr in ctrls_ls:
+			rig_ctrls.update(controls.export_ctrl_shape(ctr))
+		
+		try:
+			file_path = os.path.join(self.session_obj.paths['shapes'], 
+																'shapes.json')
+			shapes_file_write = open(file_path,'w+')
+			json.dump(
+				rig_ctrls,
+				shapes_file_write,
+				indent = 4,
+				sort_keys = True
+			)
+
+			shapes_file_write.close()
+
+			print '--- Ctrls shapes exported! ---'
+		except:
+			print 'ERROR: Could not export ctrls shape'
+	
+	def import_rig_ctrls(self):
+		""" Loads the ctrl information from shapes.json and applies to the rig
+		"""
+		if self.stage in ['template', 'init']:
+			print "###WARNING: Can't import control shapes when working on"\
+										"the template o init stages of the rig"
+		else:
+			file_path = os.path.join(
+				self.session_obj.paths['shapes'],
+				'shapes.json'
+			)
+			if os.path.isfile(file_path):
+				file_read = open(file_path, 'r')
+				ctrls_data = json.load(file_read)
+				for ctr in ctrls_data.keys():
+					if cmds.objExists(ctr):
+						for shape in ctrls_data[ctr].keys():
+							points = ctrls_data[ctr][shape]['points']
+							for i,p in enumerate(points):
+								cmds.xform('%s.cv[%i]' % (shape, i), os = 1, 
+																ws = 0, t = p)
+					else:
+						print '%s was not found, shapes not imported.' % ctr
+			else:
+				print '###WARNING: Could not find shapes.json file'
---
+++ builder/session.py
@@ -0,0 +1,347 @@
+""" Contains Session class, used to manage the asset's workspace
+"""
+
+# Sys imports
+import os
+import shutil
+
+# Module imports
+from CARF.scripts.maya_core import maya_files
+
+# Global data import
+from CARF.data import global_data as g_data
+
+# Global constants
+__fixed_paths__ = {
+	'tools_scripts': os.path.dirname(__file__),
+	'source_build': os.path.join(os.path.dirname(__file__),'asset_build.py'),
+	'components': os.path.join(os.path.dirname(__file__),'components')
+}
+__existing_projects__ = os.listdir(g_data.productions_root)
+
+class Session(object):
+	"""
+	Session class.
+	Handles the directory information needed for each asset, opening of files
+	and publishing.
+	"""
+	def __init__(self, user, project_name = None, asset_name=None, 
+			asset_type = None, rig_type = None):
+		"""
+		Args:
+			param1 (type) : Description
+		"""
+		
+		self.user_name = user
+		self.user_path = os.path.join(g_data.users_root, self.user_name)
+		self.paths = dict(__fixed_paths__)
+
+		# Set up backups directory
+		self.user_backups_path = os.path.join(self.user_path, 
+											  '__assets_backups__')
+		if not os.path.isdir(self.user_backups_path):
+			check_create_folder(self.user_backups_path)
+
+		#Empty attributes for clarity of mind
+		self.project_name = None
+		self.project_path = None
+		self.project_set = False
+
+		#Asset related attributes
+		self.asset_name = None
+		self.asset_set = False
+		self.asset_directory_created = False
+
+		if project_name:
+			self.set_project()
+		#If asset_name is provided, set the data
+		if asset_name and asset_type and rig_type:
+			self.set_asset(asset_name, asset_type, rig_type)
+
+	def set_project(self, project_name):
+
+		if not project_name in __existing_projects__:
+			raise Exception(
+				'{} is not an existing project: unable to set data.\n'\
+				'Please use create_project() to add a new project'\
+				.format(project_name)
+			)
+		
+		self.project_name = project_name
+		self.project_path = os.path.join(g_data.productions_root, project_name)
+
+		###Hard coded for now, names come from g_data.project_folders
+		self.paths['checkin'] = os.path.join(self.project_path, 
+											 'Checkins', 'Assets')
+		self.paths['final'] = os.path.join(self.project_path,
+										   'Final', 'Assets')
+
+		#List of all assets under current project
+		self.project_assets = os.listdir(self.project_path)
+
+		self.project_set = True
+	
+	def create_set_project(self, project_name):
+		self.create_project(project_name)
+		self.set_project(project_name)
+
+	def create_project(self, project_name):
+		""" Creates the folder structure for a new project
+		Args:
+			project_name (str) : Name to be used for the project.
+		"""
+		
+		project_path = os.path.join(g_data.productions_root, project_name)
+		if os.path.isdir(project_path):
+			print '##Warning: %s already exists as a project,'\
+			' no folders created' % project_name
+		else:
+			check_create_folder(project_path)
+
+		self.create_directory_tree(project_path, g_data.project_folders)
+
+		__existing_projects__.append(project_name)
+
+	def create_directory_tree(self,base_path, folders_dictionary):
+		"""Recursive method, it will create a directory tree in the given path
+		based on the given dictionary
+		Args:
+			base_path (os path) : path where to start creating directories
+			folders_dictionary (dict) : Dictionary representing the directory
+			tree. Check global_data.py for more info on folders dictionaries
+		"""
+		for folder in folders_dictionary:
+			folder_path = os.path.join(base_path, folder)
+			check_create_folder(folder_path)
+			if folders_dictionary[folder]:
+				self.create_directory_tree(folder_path, 
+					folders_dictionary[folder])
+
+	def set_asset(self, asset_name, asset_type, rig_type):
+		""" Changes the current session asset. 
+		This implies updating all the 'paths' for this session. It checks
+		whether or not this paths are valid by checking if the asset directory
+		exists
+		"""
+		if not self.project_set :
+			raise Exception('Project data not found. Please set project'\
+							' before setting the asset')
+
+		#Validation of arguments
+		if not (type(asset_name) is str or type (asset_type) is str):
+			raise Exception("Asset name and type should be a string")
+		
+		elif rig_type not in g_data.supported_rig_types:
+			raise Exception(
+				'%s is not an supported rig type. Supported '\
+				'types are: %s' % (rig_type, g_data.supported_rig_types)
+			)
+		
+		#Updating the asset name and type
+		self.asset_name = asset_name
+		self.asset_type = asset_type
+		self.rig_type = rig_type
+		
+		asset_path = os.path.join(self.user_path, asset_name)
+		
+		if not os.path.exists(asset_path):
+			self.asset_directory_created = False
+			
+		self.paths['asset'] = asset_path
+		
+		#Set geo path
+		self.paths['geo'] = os.path.join(
+			self.paths['final'], asset_type, 
+			asset_name, 'Geo', '%s.ma' % asset_name
+		)
+
+		#Add asset dev folders to paths
+		for folder in g_data.asset_dev_folders:
+			f_path = os.path.join(self.paths['asset'], folder)
+			self.paths[folder] = f_path
+		
+		build_path = os.path.join(asset_path, '%s_build.py' % self.asset_name)
+		self.paths['build'] = build_path
+		
+		self.asset_set = True
+	
+	def create_asset_workspace(self):
+		""" Creates the working directories for the current assets
+		It will create the asset_dev_folders directory tree found in 
+		global_data.py and it will create the asset_build.py file
+		"""
+		#Check for asset folder
+		asset_path=self.paths['asset']
+		check_create_folder(asset_path)
+		self.create_directory_tree(asset_path, g_data.asset_dev_folders)
+		
+		#Check for asset_build.py
+		build_path = os.path.join(asset_path, '%s_build.py' % self.asset_name)
+		if not os.path.isfile(build_path):
+			self.create_build_file(build_path, self.rig_type)
+		
+		self.asset_directory_created = True
+		
+		
+	def check_in_asset(self):
+		"""Saves the current asset dev folders into the checkins directory.
+		Checks for older versions in Checkins. If found, creates a new one to 
+		save the new data. 
+		Versions are numbers with padding of 3
+		"""
+		if not self.asset_set:
+			raise Exception('Asset data not set. Unable to check-in the data')
+		
+		#Check and creates Chekins directory
+		checkin_path = os.path.join(self.paths['checkin'], self.asset_type,
+									self.asset_name)
+		check_create_folder(checkin_path)
+
+		#Get the next version number			
+		versions = [int(x) for x in os.listdir(checkin_path)]
+		new_version = 0 if versions == [] else max(versions)+1
+		version_path = os.path.join(checkin_path, str(new_version).zfill(3))
+
+		#Copies the asset development folders into the new version checkin
+		shutil.copytree(self.paths['asset'], version_path)
+	
+	def check_out_asset(self):
+		""" Looks for the latest asset directory in the Checkins folder, then
+		makes a local copy into the current's user Asset Dev
+
+		If There is an existing asset directory in the users dev area, then it
+		moves the current directory into the Stash folder, then checks out the
+		latest asset directory
+		"""
+		if not self.asset_set:
+			raise Exception('Asset data not set. Unable to check-out the data')
+		
+		#Gathering the latest checked data
+		checkin_path = os.path.join(self.paths['checkin'], self.asset_type,
+			self.asset_name)
+		
+		if not os.path.isdir(checkin_path):
+			raise Exception(
+				'Unable to check out data: '\
+				'No checked in data found for {}'.format(self.asset_name)
+			)
+		if not len(os.listdir(checkin_path)):
+			raise Exception(
+				'Unable to check out data: '\
+				'Check in directory for {} is empty, no data to copy from'\
+				.format(self.asset_name)
+			)
+		
+		versions = [int(x) for x in os.listdir(checkin_path)]
+		latest_version = max(versions)
+		latest_checked_data = os.path.join(checkin_path, 
+										   str(latest_version).zfill(3))
+		#User Asset dev
+		user_asset_path = self.paths['asset']
+
+		# If user data is present, back it up and then remove it
+		if os.path.isdir(user_asset_path):
+			self.backup_user_asset_data()
+			shutil.rmtree(user_asset_path)
+		
+		#Copies from latest into user dev folder
+		shutil.copytree(latest_checked_data, user_asset_path)
+
+	def backup_user_asset_data(self):
+		""" Makes a copy of the user's current asset data in the 
+		__assets_backups__ folder
+
+		It saves each new backup with a bigger increment suffix. 
+		TODO: implement the following... 
+		if the amount of backups is the same as the maximum number of backups 
+		specified in global_data, delete the oldest
+		"""
+		#Look for the next backup increment and use it as the directory name
+		next_backup = len(os.listdir(self.user_backups_path)) + 1
+		backup_name = '_'.join([self.asset_name, str(next_backup).zfill(3)])
+		backup_path = os.path.join(self.user_backups_path, backup_name)
+
+		#Create the backup
+		user_asset_path = self.paths['asset']
+		shutil.copytree(user_asset_path, backup_path)
+
+	def publish_asset(self):
+		"""Checks in the asset, then saves the current maya file into the
+		publish folder. 
+		
+		If there is an existing publish, it will add it to the archive as an
+		increment.
+		"""
+		
+		self.check_in_asset()
+
+		#Check and creates the asset directory in the final drive
+		final_path = os.path.join(self.paths['final'], self.asset_type,
+								  self.asset_name)
+		check_create_folder(final_path)
+
+		rig_path = os.path.join(final_path, 'Rig')
+		check_create_folder(rig_path)			
+		
+		archive_path = os.path.join(rig_path,'Archive')
+		check_create_folder(archive_path)
+
+		#Get the next version number			
+		versions = [int(x) for x in os.listdir(archive_path)]
+		new_version = 0 if versions == [] else max(versions)+1
+		version_path = os.path.join(archive_path, str(new_version).zfill(3))
+		check_create_folder(version_path)
+
+		for folder_path in [rig_path, version_path]:
+			file_path = os.path.join(folder_path,'%s_rig.ma' % self.asset_name)
+			maya_files.save_file_as(file_path)
+		
+	def open_build(self):
+		open_in_os(self.paths['build'])
+
+	def open_asset_directory(self):
+		open_in_os(self.paths['asset'])
+
+	def create_build_file(self, build_path, asset_type):
+		'''Creates a copy of asset_build.py  in the dev folder
+		args:
+			build_path (str) : Path to build file in dev folder
+			asset_type (str) : Rig type name
+		'''
+
+		source_build = self.paths['source_build']
+		temp_path = build_path.replace('build','temp')
+
+		shutil.copy(source_build, build_path) #Copy build
+		shutil.copy(source_build, temp_path) #Temp file to replace
+
+		asset_build = open(build_path, 'w+')
+		temp_build = open(temp_path, 'r')
+
+		#rewritting asset_build with values instead of tokens
+		replace_tokens = {
+			'asset_token':self.asset_name,
+			'type_token':asset_type,
+			'Type_token':asset_type.capitalize(),
+		}
+		for l in temp_build:
+			for tok in replace_tokens.keys():
+				if tok in l:
+					l = l.replace(tok, replace_tokens[tok])
+			asset_build.write(l)
+		
+		#Close files and remove temp
+		asset_build.close()
+		temp_build.close()
+		os.remove(temp_path)
+
+		self.paths['build'] = build_path
+
+def open_in_os(path):
+	""" Relative to each OS, opens given path """
+	os.startfile(path)
+
+def check_create_folder(folder_path):
+	"""Checks for existing folder, if not found, creates it """
+	if not os.path.exists(folder_path):
+		os.mkdir(folder_path)
