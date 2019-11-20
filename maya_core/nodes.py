0a4228c1ce5354d5da2e5ab137dacc904244c375
BTCPRL <33190493+BTCPRL@users.noreply.github.com>
2019-11-20 00:04:51 -0800

    Nodes Updated to 2.0: - Now using properties to set name and node - Class dictionaries moved outside of __init__ - Removed exceptions to side prefix - Removed unused methods - Added Docstrings

--- maya_core/nodes.py
+++ maya_core/nodes.py
@@ -1,6 +1,5 @@
-import pymel.core as pm
 import maya.cmds as cmds
-
+import pymel.core as pm
 from CARF.data import global_data as g_data
 from CARF.maya_core import attributes
 
@@ -9,90 +8,122 @@ class Node(object):
     """
     Nodes class.
 
-    Creates Node objects, storing the pymel object of the corresponding type
+    Creates Node objects, storing the created pymel object
     """
 
-    def __init__(self, node_type, name, side=g_data.center):
+    """Supported types dictionary
+    Format: {name: [suffix, maya type]}
+    """
+    _known_types_ = {
+        # Utility nodes
+        'condition': ['_COND', 'condition'],
+        'remapValue': ['_RMV', 'remapValue', ],
+        'clamp': ['_CLP', 'clamp'],
+        'reverse': ['_REV', 'reverse'],
+        # Basic Math
+        'multiply': ['_MUL', 'multiplyDivide'],
+        'divide': ['_DIV', 'multiplyDivide'],
+        'add': ['_ADD', 'plusMinusAverage'],
+        'substract': ['_SUB', 'plusMinusAverage'],
+        # Matrices
+        'decomposeMatrix': ['_DMX', 'decomposeMatrix'],
+        # Curve nodes
+        'nearestPointOnCurve': ['_NPC', 'nearestPointOnCurve'],
+        'pointOnCurveInfo': ['_POC', 'pointOnCurveInfo'],
+        # Dag nodes, they get the suffix from name
+        'transform': [None, 'transform'],
+        'control': [None, 'transform'],  # Used by controls module
+        'joint': [None, 'joint'],
+    }
+    # Auto detect nodes
+    _auto_detect_ = {
+        'divide': 2,
+        'substract': 2
+    }
+
+    @property
+    def name(self):
+        """ Full name of the node
+        full name is of form {side}_{description}_{suffix}
+        """
+        return self._full_name
+
+    @name.setter
+    def name(self, name):
+        """ Setter for full name attribute. 
+        :arg:
+        name (str): Full name of node, it should include side prefix
+        """
+        if len(name.split('_')) < 2:
+            raise Exception('Node name should include a side prefix')
+
+        prefix = name.split('_')[0]
+
+        if prefix not in g_data.positions_prefifx:
+            raise Exception('{} has an invalid side prefix'.format(name))
+
+        self._full_name = name
+
+    @property
+    def node(self):
+        """ This will carry the pymel node
+        The Pymel node is not commonly used but kept for ease of access
+        """
+        return self._pm_node
+
+    @node.setter
+    def node(self, node_type):
+        """ Assigns the node, if the given node_type is a list, it will assign
+        the first item
+        """
+        if type(node_type) is list:
+            self._pm_node = node_type[0]
+        else:
+            self._pm_node = node_type
+
+    def __init__(self, name, node_type):
+        """ Instanciates the Class and creates the maya node
+        If the given node_type is supported as an auto-detect node. It will 
+        set the default attributes
+
+        :args:
+        node_type (str): Which Maya node to create.
+        name (str): Name to give the newly created node
+
+        :returns:
+        (Node) the instanciated object, that shares the name with the maya node
+        """
 
         # Node base attributes
-        self.side = side
-        self.name = name
-        self.node_type = None
-        self.full_name = None
-        self.pm_node = None
-        self.added_attributes = {}
+        self._full_name = None
+        self._pm_node = None
 
-        # side is None for ROOT and top level groups
-        if self.side:
-            if self.side not in g_data.positions_prefifx:
-                raise Exception('side has to be one of the following: '
-                                '%s' % g_data.positions_prefifx)
+        # Extract data from _known_types_ dictionary if possible
+        suffix = ''
+        maya_type = node_type
+        if node_type in Node._known_types_.keys():
+            suffix = Node._known_types_[node_type][0]
+            maya_type = Node._known_types_[node_type][1]
 
-        # Supported types dictionary
-        # Format: {name: [suffix, maya type]}
-        supported_node_types = {
-            # Utility nodes
-            'condition': ['_COND', 'condition'],
-            'remap': ['_RMV', 'remapValue', ],
-            'clamp': ['_CLP', 'clamp'],
-            'reverse': ['_REV', 'reverse'],
-            # Basic Math
-            'multiply': ['_MUL', 'multiplyDivide'],
-            'divide': ['_DIV', 'multiplyDivide'],
-            'add': ['_ADD', 'plusMinusAverage'],
-            'substract': ['_SUB', 'plusMinusAverage'],
-            # Matrices
-            'decomposeMatrix': ['_DMX', 'decomposeMatrix'],
-            # Curve nodes
-            'nearestPointOnCurve': ['_NPC', 'nearestPointOnCurve'],
-            'pointOnCurveInfo': ['_POC', 'pointOnCurveInfo'],
-            # Dag nodes, they get the suffix from name
-            'transform': [None, 'transform'],
-            'control': [None, 'transform'],  # Used by controls module
-            'joint': [None, 'joint'],
-        }
-        # Auto detect nodes
-        auto_detect_nodes = {
-            'divide': 2,
-            'substract': 2
-        }
-        # Extract data from dictionary if possible
-        if node_type in supported_node_types.keys():
-            suffix = supported_node_types[node_type][0]
-            maya_type = supported_node_types[node_type][1]
+        # Format node name
+        node_name = '{}{}'.format(name, suffix)
 
-        else:
-            suffix = None
-            maya_type = node_type
+        if cmds.objExists(node_name):
+            del(self)
+            raise RuntimeError("{} Already exists. Can't create it again")
+
+        self.name = node_name
 
         # Create node
-        node_name = name
-        if side:
-            node_name = '{}_{}'.format(side, node_name)
-        if suffix:
-            node_name = '{}_{}'.format(node_name, suffix)
+        node = pm.createNode(maya_type, n=self._full_name)
 
-        self.full_name = node_name
+        if maya_type in Node._auto_detect_:
+            attributes._set(node, 'operation', Node._auto_detect_[maya_type])
 
-        node = pm.createNode(maya_type, n=self.full_name)
-
-        if maya_type in auto_detect_nodes:
-            attributes._set(node, 'operation', auto_detect_nodes[maya_type])
-
-        self.set_pm_node(node)
+        self.node = node
 
     def __str__(self):
-        return self.full_name
-
-    def set_pm_node(self, node):
-        if type(node) is list:
-            self.pm_node = node[0]
-        else:
-            self.pm_node = node
-
-    def duplicate(self, new_name):
-        """Returns a new node of the same type, with a new name"""
-        return Node(self.node_type, new_name)
+        return self.name
 
     """
 	Attribute methods
@@ -100,42 +131,43 @@ class Node(object):
 	"""
 
     def attr_add(self, attr_name='', attr_type='slider', default_value=0,
-                 min_value=None, max_value=None, hidden=False, keyable=True, **Kwargs):
+                 min_value=None, max_value=None, hidden=False,
+                 keyable=True, **Kwargs):
         """ Creates a new attribute """
-        attributes._add(self.pm_node, attr_name, attr_type, default_value,
+        attributes._add(self._pm_node, attr_name, attr_type, default_value,
                         min_value, max_value, hidden, keyable, **Kwargs)
 
     def attr_get(self, attr_name, **Kwargs):
         """Returns the attribute value"""
-        return attributes._get(self.pm_node, attr_name, **Kwargs)
+        return attributes._get(self._pm_node, attr_name, **Kwargs)
 
     def attr_set(self, attr_name, value=None, **Kwargs):
         """Sets the specified value in the given attribute"""
-        attributes._set(self.pm_node, attr_name, value, **Kwargs)
+        attributes._set(self._pm_node, attr_name, value, **Kwargs)
 
     def attr_lock(self, attr_name, hide=True):
         """Locks the attribute"""
-        attributes._lock(self.pm_node, attr_name, hide)
+        attributes._lock(self._pm_node, attr_name, hide)
 
     def attr_unlock(self, attr_name, show=True):
         """Unlocks the attribute"""
-        attributes._lock(self.pm_node, attr_name, show, **Kwargs)
+        attributes._lock(self._pm_node, attr_name, show, **Kwargs)
 
     def attr_connect(self, attr_name, target, target_attr, f=False):
         """Connects given attribute with target"""
-        attributes._connect(self.pm_node, attr_name,
+        attributes._connect(self._pm_node, attr_name,
                             target, target_attr, f=f)
 
     def attr_link(self, attr_name, target, f=False):
         """Links same attribute betwen targets"""
-        attributes._link(self.pm_node, attr_name, target, f=f)
+        attributes._link(self._pm_node, attr_name, target, f=f)
 
     def attr_connectReverse(self, attr_name, target, target_attr, f=False):
         """Connects given attribute with target via reverse node"""
-        attributes._connectReverse(self.pm_node, attr_name,
+        attributes._connectReverse(self._pm_node, attr_name,
                                    target, target_attr, f=f)
 
     def attr_connectNegative(self, attr_name, target, target_attr, f=False):
         """Connects the negative value of the given attribute with target"""
-        attributes._connectNegative(self.pm_node, attr_name,
+        attributes._connectNegative(self._pm_node, attr_name,
                                     target, target_attr, f=f)
