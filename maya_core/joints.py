4448e90f2c4ad08461db001cbe8bede9e6917133
BTCPRL <33190493+BTCPRL@users.noreply.github.com>
2020-01-15 23:55:19 -0800

    - Simplified args - Added joint labeling

--- maya_core/joints.py
+++ maya_core/joints.py
@@ -16,7 +16,31 @@ class Joint(trans.Transform):
 
         super(Joint, self).__init__(node_type='joint', name=name, **Kwargs)
 
-        cmds.makeIdentity(self.pm_node, apply=1)
-        cmds.joint(self.pm_node, edit=True, orientJoint='xyz',
+        # Orienting the joint
+        cmds.makeIdentity(self.name, apply=1)
+        cmds.joint(self.name, edit=True, orientJoint='xyz',
                    secondaryAxisOrient='xup', children=False,
                    zeroScaleOrient=True)
+
+        # Labeling the joint
+        label_joint(self.name)
+
+
+def label_joint(jnt_name):
+    """ Sets the label value for the joint
+    It bases the side and the label on self.name. Type is always "other"
+    """
+    if jnt_name.split('_')[0] not in g_data.positions_prefifx:
+        raise ValueError('Joint labeling only works with a valid side prefix')
+
+    jnt_label = jnt_name[2:]
+    label_side_options = {
+        'L': 1,
+        'M': 0,
+        'R': 2,
+    }
+    label_side = label_side_options[jnt_name[0]]
+    cmds.setAttr('{}.type'.format(jnt_name), 18)  # Custom label
+    cmds.setAttr('{}.otherType'.format(jnt_name),
+                 jnt_label, type='string')
+    cmds.setAttr('{}.side'.format(jnt_name), label_side)
